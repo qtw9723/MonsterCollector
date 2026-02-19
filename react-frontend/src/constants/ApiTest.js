5e8cf8c2a8eeb939c675e898259958960fc2e724
sangjun <qyw9723@naver.com>
2026-02-19 17:28:27 +0900

    api test
    # Conflicts:
    #	.github/workflows/keep_alive.yml

---
+++ react-frontend/src/constants/ApiTest.js
@@ -0,0 +1,9 @@
+    
+export const MATERIAL_GRADES = ["NORMAL", "RARE", "EPIC", "LEGENDARY"];
+
+export const MATERIAL_IMAGES = {
+  NORMAL: "/materials/material_normal.png",
+  RARE: "/materials/material_rare.png",
+  EPIC: "/materials/material_epic.png",
+  LEGENDARY: "/materials/material_legend.png",
+};
